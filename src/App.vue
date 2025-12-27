e7c2e4c4be8e97a84aac32e39f45a9ad7948f011
Rosefinch-Midsummer <55123630+Rosefinch-Midsummer@users.noreply.github.com>
2025-12-27 22:54:08 +0800

    first commit to initialize the website

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import FileExplorer from './components/FileExplorer.vue';
+</script>
+
+<template>
+  <FileExplorer />
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #f4f6f8;
+}
+</style>
